a178129d30b4bf9cee8fdc468ddae5e634373c54
Lea Dousset <lea.dousset@chicagobooth.edu>
2019-09-18 15:47:29 +0000

    Solved collatz-conjecture

--- collatz-conjecture/collatz_conjecture.py
+++ collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,9 @@
 def steps(number):
-    pass
+    i=0 
+    while number!= 1: 
+        if (number%2) == 0:
+            number=(number/2)
+        else: 
+           number=(3*number+1)/2 
+        i=i+1
+    return(i)
