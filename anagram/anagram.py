020595c767e1e9d2444590be120e7a55dfb0508a
Igor Kuznetsov <americanq@gmail.com>
2019-09-18 16:29:43 +0000

    igork7 anagram solution

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -1,13 +1,20 @@
 def find_anagrams(word, candidates):
-    wordsp = word.split()
     for i in candidates:
-        test_wordsp = i.split()
-        for j in wordsp:
-            if j in test_wordsp:
-                test_wordsp.remove(j)
+        count1 = 0
+        # print(i)
+        candsp = list(i)
+        # print(candsp)
+        for j in word:
+            # print(j)
+            if j in candsp:
+                candsp.remove(j)
+                # print(candsp)
+                count1 += 1
             else:
+                #print("continue")
                 break
-        return(print(i))
+        if len(word) == count1:
+            return(print(i))
     return(print("No matches"))
     
-find_anagrams()
+find_anagrams(word, candidates)
