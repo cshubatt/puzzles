ef1f69e819c5d23ef295d4c12cac861c887b258d
Lea Dousset <lea.dousset@chicagobooth.edu>
2019-09-18 15:48:27 +0000

    Solved darts

--- darts/darts.py
+++ darts/darts.py
@@ -1,2 +1,11 @@
 def score(x, y):
-    pass
+    if x**2 + y**2 > 10**2:
+        point=0 
+    if x**2 + y**2 <= 10**2 and x**2 + y**2 > 5**2: 
+        point=1
+    if x**2 + y**2 <= 5**2 and x**2 + y**2 > 1**2: 
+        point=5   
+    if x**2 + y**2 <= 1**2: 
+        point=10
+    
+    return point
