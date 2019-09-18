ca336df8a35704a454a6aa0791f40b1c956e79c7
Igor Kuznetsov <americanq@gmail.com>
2019-09-18 15:52:13 +0000

    IgorK7 anagram temp solution

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -1,2 +1,13 @@
 def find_anagrams(word, candidates):
-    pass
+    wordsp = word.split()
+    for i in candidates:
+        test_wordsp = i.split()
+        for j in wordsp:
+            if j in test_wordsp:
+                test_wordsp.remove(j)
+            else:
+                break
+        return(print(i))
+    return(print("No matches"))
+    
+find_anagrams()
